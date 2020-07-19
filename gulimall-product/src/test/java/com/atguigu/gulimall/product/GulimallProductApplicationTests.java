b1b394a6489b71c3373e883b68f3baa3e9b37c0a
TThappy <827105427@qq.com>
2020-07-19 11:30:29 +0800

    实现common封装和Product项目逆向工程

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,28 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
+//    导入服务
+    @Autowired
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        //插入
+//        brandEntity.setName("华为");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功...");
+        //修改
+        brandEntity.setBrandId(1L);
+        brandEntity.setDescript("中华有为");
+        brandService.updateById(brandEntity);
     }
 
 }
